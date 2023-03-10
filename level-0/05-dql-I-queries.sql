3b3f4552569fb72182de6d7b81327bb26d222137
chisomloius <chisanloius@gmail.com>
2023-03-10 11:31:49 +0100

    7/10

--- level-0/05-dql-I-queries.sql
+++ level-0/05-dql-I-queries.sql
@@ -18,22 +18,6 @@
 
 */
 
-|S/N|ORDER OF WRITING|ORDER OF EXECUTION|
-|--|---|-----|
-|1|Select|From|
-|2|Top|Where|
-|3|Distinct|Group by|
-|4|From|Having|
-|5|Where|Select|
-|6|Group by|Window|
-|7|Having|QUALIFY|
-|8|Order by|Distinct|
-|9|Second|Order by|
-|10|QUALIFY|Top|
-|11|Limit|Limit|
-
-All this queries are selcted as the most common types of queries
-
 ---------------------------------------------------------------------------------------------
 
 
